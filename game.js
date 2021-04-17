fc8b323ae36d114f2d3d48ec0a6a8ec8abccf3a2
LiaToXD <liardotommaso1999@gmail.com>
2021-04-17 11:15:20 +0200

    f

--- game.js
+++ game.js
@@ -1,44 +1,71 @@
-const textElement = document.getElementById('text');
-const choicesElement = document.getElementById('choices')
+const textElement = document.getElementById('text') /* Assign the element #text to textElement for manipulation */
+const choicesElement = document.getElementById('choices') /* Assign the #choices element to choicesElement for manipulation */
 
-let state = {}
-
+/*Define*/
+let inventory = {}
+let hp = 100
+let strength = 100
+let updateText= ''
+let updateImage= ''
+ 
+/* Set hp and strength by default, inventory empty and starts the game with the first text node */
 function startGame() {
-  state = {}
-  showTextNode(1)
+	document.getElementById("hp").innerHTML = "HP: " + hp
+	document.getElementById("strength").innerHTML = "Strength: " + strength
+	inventory = {}
+	showTextNode(1)
 }
 
+/* Shows the current text node after receiving the relative index */
 function showTextNode(textNodeIndex) {
-  const textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
-  textElement.innerText = textNode.text
-  while (choicesElement.firstChild) {
-    choicesElement.removeChild(choicesElement.firstChild)
+	
+	const textNode = textNodes.find(textNode => textNode.id === textNodeIndex) 	/* Assigns the current text node from list to textNode for display */
+	textElement.innerText = textNode.text  	/* Inserts the text from current node into the #text html element */
+	
+	/* While loop removes all the options buttons so that we can add them as many as needed later for each node */
+	while (choicesElement.firstChild) {
+    choicesElement.removeChild(choicesElement.firstChild) 
   }
 
-  textNode.options.forEach(option => {
-    if (showOption(option)) {
-      const button = document.createElement('button')
-      button.innerText = option.text
-      button.classList.add('choice')
-      button.addEventListener('click', () => selectOption(option))
-      choicesElement.appendChild(button)
-    }
-  })
+/* Creates the buttons with the relative options */
+textNode.options.forEach(option => {
+    const button = document.createElement('button')
+    button.innerText = option.text /* Inserts text in each button for each option available */
+    button.classList.add('choice')
+    button.addEventListener('click', () => selectOption(option)) /* Adds event listener to button and links it to the selectOption() function*/
+    choicesElement.appendChild(button) /* Insert the button in the choices div */
+  }
+ )
 }
 
-function showOption(option) {
-  return option.requiredState == null || option.requiredState(state)
-}
-
+/* Function to set new text and options after selecting an option */
 function selectOption(option) {
-  const nextTextNodeId = option.nextText
+  const nextTextNodeId = option.nextText /* Set nextTextNodeId to contain the text for the next step*/
+  
+  /* Check if the text ID is negative, which is only held by "restart" options when game is over, restarts the game and resets stats */
   if (nextTextNodeId <= 0) {
     return startGame()
+	var hp= 100
+	var strength = 100
   }
-  state = Object.assign(state, option.setState)
-  showTextNode(nextTextNodeId)
+  
+  /* inventory = Object.assign(inventory, option.setState) */
+  showTextNode(nextTextNodeId) /* Call showTextNode() function to pass onto the next node */
+  hp = option.setHP /* Update hp value after choosing an option */
+  updateText = option.setUpdate
+  updateImage = option.setImage
+  console.log(updateText)
+  /* Rewrite values into the HP and strength sections*/
+  document.getElementById("hp").innerHTML = "HP: " + hp
+  document.getElementById("strength").innerHTML = "Strength: " + strength
+  document.getElementById("info").innerHTML =  "" + updateText
+  document.getElementById('currentImage').src = updateImage
+  
 }
 
+
+/* List with nodes. Each node will have ID, explanation text and 1 to 4 possible options that can link to different nodes or end the game. 
+Each option can also modify the HP/strength value and/or add items to inventory*/
 const textNodes = [
   {
     id: 1,
@@ -46,8 +73,10 @@ const textNodes = [
     options: [
       {
         text: 'option 1',
-        /*setState: {}*/
-        nextText: 2
+		setHP: 80,
+        nextText: 2,
+		setUpdate: 'You lost 20 HP!',
+		setImage: 'test.jpg'
       },
       {
         text: 'option 2',
@@ -61,14 +90,13 @@ const textNodes = [
     options: [
       {
         text: 'option 1',
-        /*requiredState: (currentState) => currentState.blueGoo,
-        setState: { blueGoo: false, sword: true }*/
-        nextText: 3
+        nextText: 3,
+		setHP: 100,
+		setUpdate: 'You gained 20hp',
+		setImage: 'Mansion.jpg'
       },
       {
         text: 'option 2',
-        /*requiredState: (currentState) => currentState.blueGoo,
-        setState: { blueGoo: false, shield: true }*/
         nextText: 3
       },
       {
@@ -135,17 +163,14 @@ const textNodes = [
       },
       {
         text: 'option 2',
-        /*requiredState: (currentState) => currentState.sword,*/
         nextText: 9
       },
       {
         text: 'option 3',
-        /*requiredState: (currentState) => currentState.shield,*/
         nextText: 10
       },
       {
         text: 'option 4',
-        /*requiredState: (currentState) => currentState.blueGoo,*/
         nextText: 11
       }
     ]
@@ -192,4 +217,5 @@ const textNodes = [
   }
 ]
 
+/* Start the game */
 startGame();
